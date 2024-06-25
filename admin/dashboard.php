f4a74f84fc9019cbc4116d0a0507823847ee10ac
Angelina Suhartono <322310006@student.machung.ac.id>
2024-06-25 17:18:13 +0700

    add: page in folder admin

---
+++ admin/dashboard.php
@@ -0,0 +1,17 @@
+<?php require_once '../lib/seo.php'; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <?php SEO("Dashboard | ERIGO STORE"); ?>
+</head>
+
+<body>
+  <main class="w-full h-screen font-[Poppins]">
+    <?php include_once  '../components/core/header.php'; ?>
+    <?php include_once  '../components/core/sidebar.php'; ?>
+  </main>
+</body>
+
+</html>
